4bd7ab6a7a3b0b81ec38c204af5c242e5ea21f46
bchbchbch <1924187+bchbchbch@users.noreply.github.com>
2018-03-20 11:46:29 -0700

    Fix HTTP (#19)
    * fix http
    
    * log error

--- pkg/util/http.go
+++ pkg/util/http.go
@@ -2,11 +2,14 @@ package util
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
 // WriteHTTPError writes an HTTP error to an http.ResponseWriter
 func WriteHTTPError(w http.ResponseWriter, err error) {
-	w.Write([]byte(fmt.Sprintf(`{"status": 500, "error": "%v"}`, err)))
+	log.Error(err)
+
 	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte(fmt.Sprintf(`{"status": 500, "error": "%v"}`, err)))
 }
