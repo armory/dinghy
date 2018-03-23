a1ae777824dabe12cd5e05fc2be5ce9a87eb0149
bchbchbch <1924187+bchbchbch@users.noreply.github.com>
2018-03-23 14:19:03 -0700

    add module multilevel dependency tests (#23)

--- pkg/dinghyfile/render_test.go
+++ pkg/dinghyfile/render_test.go
@@ -4,6 +4,8 @@ import (
 	"strings"
 	"testing"
 
+	"encoding/json"
+
 	"github.com/armory-io/dinghy/pkg/cache"
 	"github.com/armory-io/dinghy/pkg/git/github"
 	"github.com/stretchr/testify/assert"
@@ -63,4 +65,60 @@ func TestSimpleWaitStage(t *testing.T) {
 	exp := strings.Join(strings.Fields(expected), "")
 	actual := strings.Join(strings.Fields(buf.String()), "")
 	assert.Equal(t, exp, actual)
+}
+
+type MultilevelFileService struct{}
+
+// Download a file from github.
+func (f *MultilevelFileService) Download(org, repo, file string) (string, error) {
+	switch file {
+	case "dinghyfile":
+		return `{{ module "wait.stage.module" "foo" "baz" }}`, nil
+
+	case "wait.stage.module":
+		return `{
+			"foo": "bar",
+			"nested": {{ module "wait.dep.module" "waitTime" 100 }}
+		}`, nil
+
+	case "wait.dep.module":
+		return `{
+			"waitTime": 4243
+		}`, nil
+	}
+
+	return "", nil
+}
+
+// GitURL returns the git url for a given org, repo, path
+func (f *MultilevelFileService) GitURL(org, repo, path string) string {
+	return (&github.FileService{}).GitURL(org, repo, path)
+}
+
+// ParseGitURL takes a url and returns the org, repo, path
+func (f *MultilevelFileService) ParseGitURL(url string) (org, repo, path string) {
+	return (&github.FileService{}).ParseGitURL(url)
+}
+
+func TestModuleVariableSubstitution(t *testing.T) {
+	cache.C = cache.NewCache()
+	f := MultilevelFileService{}
+
+	file, err := f.Download("org", "repo", "dinghyfile")
+	assert.Equal(t, nil, err)
+
+	ret := Render(cache.C, "dinghyfile", file, "org", "repo", &f)
+
+	type testStruct struct {
+		Foo    string `json:"foo"`
+		Nested struct {
+			WaitTime int `json:"waitTime"`
+		} `json:"nested"`
+	}
+
+	var ts = testStruct{}
+	err = json.Unmarshal(ret.Bytes(), &ts)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, "baz", ts.Foo)
+	assert.Equal(t, 100, ts.Nested.WaitTime)
 }
