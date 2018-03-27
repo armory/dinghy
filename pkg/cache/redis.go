456011cf6e288bce05652b6224859ebb775bdc84
Brandon Hsiao <bh@brandonhsiao.com>
2018-03-27 13:48:56 -0700

    add test for redis, got working

--- pkg/cache/redis.go
+++ pkg/cache/redis.go
@@ -1,10 +1,11 @@
 package cache
 
 import (
-	"github.com/armory-io/dinghy/pkg/settings"
-	"github.com/go-redis/redis"
 	"fmt"
 	"strings"
+
+	"github.com/armory-io/dinghy/pkg/util"
+	"github.com/go-redis/redis"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -14,8 +15,10 @@ type RedisCacheStore redis.Client
 var redisOptions redis.Options
 
 func init() {
-	redisOptions.Addr = settings.S.RedisServer
-	redisOptions.Password = settings.S.RedisPassword
+	redisHost := util.GetenvOrDefault("REDIS_HOST", "redis")
+	redisPort := util.GetenvOrDefault("REDIS_PORT", "6379")
+	redisOptions.Addr = fmt.Sprintf("%s:%s", redisHost, redisPort)
+	redisOptions.Password = util.GetenvOrDefault("REDIS_PASSWORD", "")
 	redisOptions.DB = 0
 }
 
@@ -24,8 +27,8 @@ func compileKey(keys ...string) string {
 }
 
 // NewRedisCacheStore initializes a new cache
-func NewRedisCacheStore() RedisCacheStore {
-	return RedisCacheStore{}
+func NewRedisCacheStore() *RedisCacheStore {
+	return (*RedisCacheStore)(redis.NewClient(&redisOptions))
 }
 
 func (c RedisCacheStore) SetDeps(parent string, deps ...string) {
@@ -37,39 +40,98 @@ func (c RedisCacheStore) SetDeps(parent string, deps ...string) {
 		return
 	}
 
+	// suppose current deps are (a, b, c) and new deps are (c, d, e)
+
+	// generate (a, b)
 	toDelete := make(map[string]bool, 0)
 	for _, currentDep := range currentDeps {
 		toDelete[currentDep] = true
 	}
-
 	for _, dep := range deps {
 		delete(toDelete, dep)
 	}
-
-	keysToDelete := make([]interface{}, 0)
+	depsToDelete := make([]interface{}, 0)
 	for key := range toDelete {
-		keysToDelete = append(keysToDelete, key)
+		depsToDelete = append(depsToDelete, key)
 	}
 
-	keysToAdd := make([]interface{}, 0)
+	// generate (d, e)
+	toAdd := make(map[string]bool, 0)
+	for _, dep := range deps {
+		toAdd[dep] = true
+	}
+	for _, currentDep := range currentDeps {
+		delete(toAdd, currentDep)
+	}
+	depsToAdd := make([]interface{}, 0)
 	for _, key := range deps {
-		keysToAdd = append(keysToAdd, key)
+		depsToAdd = append(depsToAdd, key)
 	}
 
 	key = compileKey("dinghy", "children", parent)
-	c.SRem(key, keysToDelete...)
-	c.SAdd(key, keysToAdd...)
+	c.SRem(key, depsToDelete...)
+	c.SAdd(key, depsToAdd...)
 
-	// TODO: finish this
+	for _, dep := range depsToDelete {
+		key = compileKey("dinghy", "parents", dep.(string))
+		c.SRem(key, parent)
+	}
+
+	for _, dep := range depsToAdd {
+		key = compileKey("dinghy", "parents", dep.(string))
+		c.SAdd(key, parent)
+	}
 }
 
-// UpstreamNodes returns the upstream nodes and root nodes for a given node
-func (c RedisCacheStore) UpstreamURLs(n *Node) (upstreams, roots []*Node) {
-	// TODO
-	return nil, nil
+func (c RedisCacheStore) UpstreamURLs(url string) (upstreams, roots []string) {
+	upstreams = make([]string, 0)
+	roots = make([]string, 0)
+
+	visited := map[string]bool{}
+	q := make([]string, 0)
+	q = append(q, url)
+
+	for len(q) > 0 {
+		curr := q[0]
+		q = q[1:]
+
+		visited[curr] = true
+
+		key := compileKey("dinghy", "parents", curr)
+		parents, err := c.SMembers(key).Result()
+		if err != nil {
+			log.Error(err)
+			break
+		}
+
+		if curr != url {
+			upstreams = append(upstreams, curr)
+			if len(parents) == 0 {
+				roots = append(roots, curr)
+			}
+		}
+
+		for _, parent := range parents {
+			if _, exists := visited[parent]; !exists {
+				q = append(q, parent)
+				visited[parent] = true
+			}
+		}
+	}
+
+	return
 }
 
 // Dump prints the cache, used for debugging
 func (c RedisCacheStore) Dump() {
 	// TODO
+}
+
+// Clear clears everything
+func (c RedisCacheStore) Clear() {
+	keys, _ := c.Keys("Armory:dinghy:children:*").Result()
+	c.Del(keys...)
+
+	keys, _ = c.Keys("Armory:dinghy:parents:*").Result()
+	c.Del(keys...)
 }
---
+++ pkg/cache/redis_test.go
@@ -0,0 +1,24 @@
+package cache
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/sirupsen/logrus"
+)
+
+func TestBlahBlah(t *testing.T) {
+	c := NewRedisCacheStore()
+	c.Clear()
+
+	c.SetDeps("df1", "mod1", "mod2")
+	c.SetDeps("mod1", "mod3", "mod4")
+
+	upstreams, roots := c.UpstreamURLs("mod4")
+	assert.EqualValuesf(t, []string{"mod1", "df1"}, upstreams, "mod4 should have parents mod1, df1")
+	assert.EqualValuesf(t, []string{"df1"}, roots, "mod4 should have roots df1")
+
+	c.SetDeps("mod1", "mod3")
+
+	_, roots = c.UpstreamURLs("mod4")
+	assert.EqualValuesf(t, []string{}, roots, "mod4 should have no roots")
+}
