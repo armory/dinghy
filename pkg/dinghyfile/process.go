0442a87ab83a8b759fd3f29d97372c2bb65cc8e8
Sandeep Raju <srand48@gmail.com>
2018-03-28 11:27:23 -0700

    updated debug

--- pkg/dinghyfile/process.go
+++ pkg/dinghyfile/process.go
@@ -32,6 +32,7 @@ func DownloadAndUpdate(p git.Push, f git.Downloader) error {
 		}
 		log.Info("Downloaded: ", file)
 		buf := Render(cache.C, settings.S.DinghyFilename, file, p.Org(), p.Repo(), f)
+		log.Debug("JSON str: ", buf.String())
 
 		d := Dinghyfile{}
 		err = json.Unmarshal(buf.Bytes(), &d)
@@ -40,8 +41,7 @@ func DownloadAndUpdate(p git.Push, f git.Downloader) error {
 			p.SetCommitStatus(status.Failure)
 			return ErrMalformedJSON
 		}
-		//log.Info("Unmarshalled: ", d)
-		log.Info("####", buf.String())
+		log.Info("Unmarshalled: ", d)
 		// todo: rebuild template
 		// todo: validate
 		if p.IsMaster() == true {
