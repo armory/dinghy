ffca4662938531c98382df4c8a8f221a6674727f
Ethan Rogers <ethanfrogers@gmail.com>
2019-01-21 19:30:27 -0500

    refactor(test): refactor tests
    refactor tests to be table driven with less duplication

--- pkg/preprocessor/preprocessor_test.go
+++ pkg/preprocessor/preprocessor_test.go
@@ -7,24 +7,64 @@ import (
 )
 
 func TestPreprocess(t *testing.T) {
-	input := `{"foo": {{ module "preprod_deploy.pipeline.module" "application" "test-spinnaker" "name" "deploy-preprod" "parameterConfig" [{"default": "selfserviceauthorization"}] }}}`
-	expected := `{"foo": {{ module "preprod_deploy.pipeline.module" "application" "test-spinnaker" "name" "deploy-preprod" "parameterConfig" "[{\"default\": \"selfserviceauthorization\"}]" }}}`
-	actual, _ := Preprocess(input)
-	assert.Equal(t, expected, actual)
-}
+	cases := map[string]struct {
+		input        string
+		expected     string
+		expectsError bool
+	}{
+		"main": {
+			input:    `{"foo": {{ module "preprod_deploy.pipeline.module" "application" "test-spinnaker" "name" "deploy-preprod" "parameterConfig" [{"default": "selfserviceauthorization"}] }}}`,
+			expected: `{"foo": {{ module "preprod_deploy.pipeline.module" "application" "test-spinnaker" "name" "deploy-preprod" "parameterConfig" "[{\"default\": \"selfserviceauthorization\"}]" }}}`,
+		},
+		"main2": {
+			input:    `{"foo": {{ module "deployments_tracker_event.stage.module" "master" "preprod"  "parameters" {"build_user_email": "SPINNAKER", "publish_event_to_deployment_tracker": true, "region": "us_east", "service_name": "${parameters.service}", "service_version": "${parameters.version}", "failure_reason": "SMOKE_TESTS", "deployment_event_value": "SOFT_FAILURE" } "stageEnabled" {"expression": "${ #stage('us-east-1 Deployment Test')['status'] != \"SUCCEEDED\"}", "type": "expression"} "requisiteStageRefIds"  ["deploytestuseast1"] "refId" "dtraktestfailureuseast1" "name" "us-east-1 Deployment Tests Failure" }}}`,
+			expected: `{"foo": {{ module "deployments_tracker_event.stage.module" "master" "preprod" "parameters" "{\"build_user_email\": \"SPINNAKER\", \"publish_event_to_deployment_tracker\": true, \"region\": \"us_east\", \"service_name\": \"${parameters.service}\", \"service_version\": \"${parameters.version}\", \"failure_reason\": \"SMOKE_TESTS\", \"deployment_event_value\": \"SOFT_FAILURE\" }" "stageEnabled" "{\"expression\": \"${ #stage('us-east-1 Deployment Test')['status'] != \\\"SUCCEEDED\\\"}\", \"type\": \"expression\"}" "requisiteStageRefIds" "[\"deploytestuseast1\"]" "refId" "dtraktestfailureuseast1" "name" "us-east-1 Deployment Tests Failure" }}}`,
+		},
+		"elvis": {
+			input:    `{"foo": {{ var "asdf" ?: 42 }}}`,
+			expected: `{"foo": {{ var "asdf" 42 }}}`,
+		},
+		"expectsError": {
+			input: `{
+				"foo": {{ module "asdf" }
+				}`,
+			expected:     "",
+			expectsError: true,
+		},
+		"multiLineAction": {
+			input: `{
+				"application": "maskednumbers",
+				"pipelines": [
+					{{
+					module "integrate.pipeline.module"
+						"application" "maskednumbers"
+						"job" "platform/job/maskednumbers/job/maskednumbers_integrate"
+						"triggerApp" "maskednumbers"
+						"triggerPipeline" "deploy-preprod"
+					}}
+				]
+				}`,
+			expected: `{
+				"application": "maskednumbers",
+				"pipelines": [
+					{{ module "integrate.pipeline.module" "application" "maskednumbers" "job" "platform/job/maskednumbers/job/maskednumbers_integrate" "triggerApp" "maskednumbers" "triggerPipeline" "deploy-preprod" }}
+				]
+				}`,
+		},
+	}
 
-func TestPreprocessAgain(t *testing.T) {
-	input := `{"foo": {{ module "deployments_tracker_event.stage.module" "master" "preprod"  "parameters" {"build_user_email": "SPINNAKER", "publish_event_to_deployment_tracker": true, "region": "us_east", "service_name": "${parameters.service}", "service_version": "${parameters.version}", "failure_reason": "SMOKE_TESTS", "deployment_event_value": "SOFT_FAILURE" } "stageEnabled" {"expression": "${ #stage('us-east-1 Deployment Test')['status'] != \"SUCCEEDED\"}", "type": "expression"} "requisiteStageRefIds"  ["deploytestuseast1"] "refId" "dtraktestfailureuseast1" "name" "us-east-1 Deployment Tests Failure" }}}`
-	expected := `{"foo": {{ module "deployments_tracker_event.stage.module" "master" "preprod" "parameters" "{\"build_user_email\": \"SPINNAKER\", \"publish_event_to_deployment_tracker\": true, \"region\": \"us_east\", \"service_name\": \"${parameters.service}\", \"service_version\": \"${parameters.version}\", \"failure_reason\": \"SMOKE_TESTS\", \"deployment_event_value\": \"SOFT_FAILURE\" }" "stageEnabled" "{\"expression\": \"${ #stage('us-east-1 Deployment Test')['status'] != \\\"SUCCEEDED\\\"}\", \"type\": \"expression\"}" "requisiteStageRefIds" "[\"deploytestuseast1\"]" "refId" "dtraktestfailureuseast1" "name" "us-east-1 Deployment Tests Failure" }}}`
-	actual, _ := Preprocess(input)
-	assert.Equal(t, expected, actual)
-}
+	for testName, c := range cases {
+		t.Run(testName, func(t *testing.T) {
+			a, err := Preprocess(c.input)
 
-func TestElvisOperator(t *testing.T) {
-	input := `{"foo": {{ var "asdf" ?: 42 }}}`
-	expected := `{"foo": {{ var "asdf" 42 }}}`
-	actual, _ := Preprocess(input)
-	assert.Equal(t, expected, actual)
+			if err != nil && c.expectsError {
+				return
+			} else if err != nil && !c.expectsError {
+				t.Fatalf("got an error but didn't expect one: %s", err.Error())
+			}
+			assert.Equal(t, c.expected, a)
+		})
+	}
 }
 
 func TestPreprocessingGlobalVars(t *testing.T) {
@@ -44,35 +84,4 @@ func TestPreprocessingGlobalVars(t *testing.T) {
 	assert.True(t, ok, "Something went wrong while extracting global vars")
 	assert.Contains(t, gvMap, "system")
 	assert.Equal(t, gvMap["system"], "order_tracking")
-}
-
-func TestMalformedAction(t *testing.T) {
-	input := `{
-				"foo": {{ module "asdf" }
-			  }`
-	_, err := Preprocess(input)
-	assert.Error(t, err, "Preprocessor didn't return error for malformed template action")
-}
-
-func TestMultiLineAction(t *testing.T) {
-	input := `{
-		"application": "maskednumbers",
-		"pipelines": [
-		  {{
-			module "integrate.pipeline.module"
-			  "application" "maskednumbers"
-			  "job" "platform/job/maskednumbers/job/maskednumbers_integrate"
-			  "triggerApp" "maskednumbers"
-			  "triggerPipeline" "deploy-preprod"
-		  }}
-		]
-	  }`
-	expected := `{
-		"application": "maskednumbers",
-		"pipelines": [
-		  {{ module "integrate.pipeline.module" "application" "maskednumbers" "job" "platform/job/maskednumbers/job/maskednumbers_integrate" "triggerApp" "maskednumbers" "triggerPipeline" "deploy-preprod" }}
-		]
-	  }`
-	actual, _ := Preprocess(input)
-	assert.Equal(t, expected, actual)
 }
