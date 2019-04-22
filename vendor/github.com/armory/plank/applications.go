4ac979ced8389e8020c635c67a3abfb10c0adb57
Paul <paul@armory.io>
2019-04-22 12:42:39 -0700

    bug(plank): Update plank to pull in bugfix

--- vendor/github.com/armory/plank/applications.go
+++ vendor/github.com/armory/plank/applications.go
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2019 Armory, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package plank
 
 import (
@@ -39,9 +54,15 @@ func (c *Client) GetApplications() (*[]Application, error) {
 	return &apps, nil
 }
 
+type createApplicationTask struct {
+	Application Application `json:"application" mapstructure:"application"`
+	Type        string      `json:"type" mapstructure:"type"`
+}
+
 // CreateApplication does what it says.
 func (c *Client) CreateApplication(a *Application) error {
-	ref, err := c.CreateTask(a.Name, fmt.Sprintf("Create Application: %s", a.Name), a)
+	payload := createApplicationTask{Application: *a, Type: "createApplication"}
+	ref, err := c.CreateTask(a.Name, fmt.Sprintf("Create Application: %s", a.Name), payload)
 	if err != nil {
 		return fmt.Errorf("could not create application - %v", err)
 	}
