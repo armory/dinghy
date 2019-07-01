70be35dc9d0ad6105185cd7c73ad2fe44a6c416a
jeffhart-armory <46694408+jeffhart-armory@users.noreply.github.com>
2019-07-01 09:50:58 -0700

    chore(yml): add yaml annotations to Dinghyfile struct (#39)

--- pkg/dinghyfile/builder.go
+++ pkg/dinghyfile/builder.go
@@ -67,11 +67,11 @@ type Downloader interface {
 type Dinghyfile struct {
 	// Application name can be specified either in top-level "application" or as a key in "spec"
 	// We don't want arbitrary application properties in the top-level Dinghyfile so we put them in .spec
-	Application          string                 `json:"application"`
-	ApplicationSpec      plank.Application      `json:"spec"`
-	DeleteStalePipelines bool                   `json:"deleteStalePipelines"`
-	Globals              map[string]interface{} `json:"globals"`
-	Pipelines            []plank.Pipeline       `json:"pipelines"`
+	Application          string                 `json:"application" yaml:"application"`
+	ApplicationSpec      plank.Application      `json:"spec" yaml:"spec"`
+	DeleteStalePipelines bool                   `json:"deleteStalePipelines" yaml:"deleteStalePipelines"`
+	Globals              map[string]interface{} `json:"globals" yaml:"globals"`
+	Pipelines            []plank.Pipeline       `json:"pipelines" yaml:"pipelines"`
 }
 
 func NewDinghyfile() Dinghyfile {
