b1efab5db7830f77d16e549799e459ba3f658206
Jeff Hart <jeffrey.hart@armory.io>
2019-07-02 22:41:56 -0700

    chore(plank): update plank

--- vendor/github.com/armory/plank/permissions.go
+++ vendor/github.com/armory/plank/permissions.go
@@ -21,17 +21,17 @@ import (
 
 // User is returned by Fiat's /authorize endpoint.
 type User struct {
-	Name         string          `json:"name"`
-	Admin        bool            `json:"admin"`
-	Accounts     []Authorization `json:"accounts"`
-	Applications []Authorization `json:"applications"`
+	Name         string          `json:"name" yaml:"name" hcl:"name"`
+	Admin        bool            `json:"admin" yaml:"admin" hcl:"admin"`
+	Accounts     []Authorization `json:"accounts" yaml:"accounts" hcl:"accounts"`
+	Applications []Authorization `json:"applications" yaml:"applications" hcl:"applications"`
 }
 
 // Authorization describes permissinos for an account or application.
 type Authorization struct {
-	Name string `json:"name"`
+	Name string `json:"name" yaml:"name" hcl:"name"`
 	// Authorizations can be 'READ' 'WRITE'
-	Authorizations []string `json:"authorizations"`
+	Authorizations []string `json:"authorizations" yaml:"authorizations" hcl:"authorizations"`
 }
 
 // IsAdmin returns true if the user has admin permissions
