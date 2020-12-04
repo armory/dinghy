1121ee28cccbf95ebb0099b426f53ff7a8f9899d
Jossuecito <jossue@gmail.com>
2020-12-04 13:57:33 -0600

    shell stuff

--- liquibase/liquibase-upgrade.sh
+++ liquibase/liquibase-upgrade.sh
@@ -6,7 +6,36 @@ DBNAME=$(yq r /opt/spinnaker/config/dinghy.yml sql.databaseName)
 DBUSER=$(yq r /opt/spinnaker/config/dinghy.yml sql.user)
 DBPASSWORD=$(yq r /opt/spinnaker/config/dinghy.yml sql.password)
 
-#I need to add validations for empty values since all are mandatory
+
+if [ "$DBENABLED" != "true" ]
+then
+  echo "SQL is not enabled so Liquibase will not be executed"
+  exit 0
+fi
+
+if [ -z "$BASEURL" ]
+then
+  echo "Value sql.baseUrl cannot be empty"
+  exit 1
+fi
+
+if [ -z "$DBNAME" ]
+then
+  echo "Value sql.databaseName cannot be empty"
+  exit 1
+fi
+
+if [ -z "$DBUSER" ]
+then
+  echo "Value sql.user cannot be empty"
+  exit 1
+fi
+
+if [ -z "$DBPASSWORD" ]
+then
+  echo "Value sql.password cannot be empty"
+  exit 1
+fi
 
 liquibase \
 --classpath="/liquibase/lib/mysql.jar" \
@@ -15,3 +44,4 @@ liquibase \
 --url="jdbc:mysql://$BASEURL/$DBNAME" \
 --username=$DBUSER \
 --password=$DBPASSWORD update
+
