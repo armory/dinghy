206bf27af4554f4d2d25ab38751ea9cb12009038
Jossuecito <jossue@gmail.com>
2021-01-22 13:24:39 -0600

    feat(appmetadata): metadata will be in root

--- vendor/github.com/armory/plank/v3/applications.go
+++ vendor/github.com/armory/plank/v3/applications.go
@@ -16,8 +16,10 @@
 package plank
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/mitchellh/mapstructure"
 	"time"
 )
 
@@ -59,6 +61,19 @@ type Application struct {
 	DataSources   *DataSourcesType   `json:"dataSources,omitempty" mapstructure:"dataSources" yaml:"datasources,omitempty" hcl:"datasources,omitempty"`
 	Permissions   *PermissionsType   `json:"permissions,omitempty" mapstructure:"permissions" yaml:"permissions,omitempty" hcl:"permissions,omitempty"`
 	Notifications NotificationsType  `json:"notifications,omitempty" mapstructure:"notifications" yaml:"notifications,omitempty" hcl:"notifications,omitempty"`
+	Metadata	  map[string]interface{}  `json:"metadata,omitempty" mapstructure:"metadata" yaml:"metadata,omitempty" hcl:"metadata,omitempty"`
+}
+
+
+// All in Metadata will be in root
+func (a Application) MarshalJSON() ([]byte, error) {
+	amap := make(map[string]interface{})
+	mapstructure.Decode(a, &amap)
+	delete(amap, "metadata")
+	for key,val := range a.Metadata {
+		amap[key] = val
+	}
+	return json.Marshal(amap)
 }
 
 // GetApplication returns the Application data struct for the
